a120a9dad74503aa943262dd06ec008b93d58d8f
Fatma Nur <fatmanurtml@gmail.com>
2022-02-06 01:58:41 +0300

    Create overloading

---
+++ overloading/Addition.java
@@ -0,0 +1,11 @@
+public class Addition {
+
+    public int add(int num1, int num2){
+        return num1+num2;
+    }
+
+    //method overloading
+    public int add(int num1, int num2, int num3){
+        return num1+num2+num3;
+    }
+}
---
+++ overloading/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Addition addition = new Addition();
+
+        System.out.println(addition.add(3,4));
+        System.out.println(addition.add(3,4,5));
+    }
+}
